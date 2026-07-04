d24a95524a32cbaaa254a943ba8f754c9e428e59
gitmaro631 <cam1998goo@gmail.com>
2026-07-05 08:44:44 +0900

    revert: restore wallet_address scope in Pi.authenticate

--- pi-sdk.js
+++ pi-sdk.js
@@ -64,7 +64,7 @@ export async function initPiSDK() {
 
 export async function authenticate() {
   return new Promise((resolve, reject) => {
-    Pi.authenticate(['username', 'payments'], onIncompletePaymentFound)
+    Pi.authenticate(['username', 'payments', 'wallet_address'], onIncompletePaymentFound)
       .then(async auth => {
         currentUser = auth.user;
 
